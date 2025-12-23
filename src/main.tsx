7978e1643aa250664e4356bb30716f95e43415f5
streamseg <it@streamsm.org>
2025-12-23 12:23:34 +0200

    Add .map() interceptor in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// EMERGENCY: Intercept .map() calls on non-arrays to debug production error
+const originalMap = Array.prototype.map;
+// @ts-ignore
+Array.prototype.map = function(...args) {
+  if (!Array.isArray(this)) {
+    console.error('[CRITICAL] .map() called on non-array:', {
+      type: typeof this,
+      value: this,
+      isArray: Array.isArray(this),
+      stack: new Error().stack
+    });
+    // Return empty array to prevent crash
+    return [];
+  }
+  // @ts-ignore
+  return originalMap.apply(this, args);
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
